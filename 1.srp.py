d8d46ef6e4368e37d742c12fc75e30a99bbc1e9d
kalleoOuriques <90047255+kalleoOuriques@users.noreply.github.com>
2021-12-07 11:39:36 -0300

    Update 1.srp.py

--- 1.srp.py
+++ 1.srp.py
@@ -1,12 +1,11 @@
 """
 Single Responsibility Principle
-
 Uma classe deve ter somente uma responsabilidade
 ou
 Uma classe deve ter somente um motivo para mudar
 """
 
-class Animal:
+'''class Animal:
     def __init__(self, name: str):
         self.name = name
     
@@ -15,4 +14,19 @@ class Animal:
 
     # salva no DB
     def save(self, animal: Animal):
+        pass'''
+
+class Animal:
+    def __init__(self, name: str, system: Sistema):
+        self.name = name
+        self.system = system
+    
+    def get_name(self) -> str:
+        pass
+
+class Sistema:
+    def __init__(self):
+        pass
+    
+    def save(self,animal: Animal):
         pass
